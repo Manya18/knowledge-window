fb047e976b71d704481432f759842d86d8a8e30a
Manya18 <61350844+Manya18@users.noreply.github.com>
2024-11-27 11:26:58 +0400

    added createAssistantPage

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CreateAssistant from './pages/createAssistant/CreateAssistant';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/createAssistant",
+    element: <CreateAssistant></CreateAssistant>,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ frontend/src/pages/createAssistant/CreateAssistant.tsx
@@ -0,0 +1,8 @@
+import styles from './createAssistant.module.css'
+
+const CreateAssistant = () => {
+    return(
+        <div className={styles.createAssistant}></div>
+    )
+}
+export default CreateAssistant;
