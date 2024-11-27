9dae4e858c9c076984915c7af5713243630cb101
dariadariadariadariadaria <149558714+dariadariadariadariadaria@users.noreply.github.com>
2024-11-27 16:02:35 +0400

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./routes/routes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
