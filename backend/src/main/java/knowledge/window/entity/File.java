d465d7e5877c57f06252887c9840607c945e34f1
gzesv <grom_zetom@mail.ru>
2024-11-28 05:12:05 +0400

    fix

--- backend/src/main/java/knowledge/window/entity/File.java
+++ backend/src/main/java/knowledge/window/entity/File.java
@@ -1,5 +1,6 @@
 package knowledge.window.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,6 +21,7 @@ public class File {
 
     @ManyToOne
     @JoinColumn(name = "assistant_id")
+    @JsonIgnore
     private Assistant assistant;
 
     @Column(name = "name")
